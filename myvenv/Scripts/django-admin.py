0adde68417e42e2d804225cdc1824f365b15d1ee
Kirby Vaughn <64096766+kirbyVaughn@users.noreply.github.com>
2020-10-27 20:25:27 -0500

    backend

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\kirby\onedrive\desktop\school_work\isqa_4900\backend_python\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
